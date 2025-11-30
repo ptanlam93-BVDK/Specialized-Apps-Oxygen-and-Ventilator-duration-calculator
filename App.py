348a0648d0a15834ba370f8b37f2bee34ed27143
ptanlam93-BVDK <ptanlam1993@gmail.com>
2025-11-30 23:07:49 +0700

    Update App.py

--- App.py
+++ App.py
@@ -452,7 +452,7 @@ with tab_oxy:
         )
 
     if st.button("✅ TÍNH GIỜ THỞ OXY ( Một khoảng)"):
-                tong_phut_oxy, err_oxy = tinh_phut(bd_oxy, kt_oxy)
+        tong_phut_oxy, err_oxy = tinh_phut(bd_oxy, kt_oxy)
 
         if err_oxy:
             st.error("⛔ " + err_oxy)
@@ -489,38 +489,8 @@ with tab_oxy:
                 </div>
                 """,
                 unsafe_allow_html=True,
-            )          st.markdown("---")
+            )
 
-        st.markdown(
-            f"""
-            <div style="
-                text-align:center;
-                padding:18px;
-                border:2px solid red;
-                border-radius:14px;
-                background-color:#1E90FF;
-            ">
-                <div style="font-size:22px; color:#FFFFFF; font-weight:600;">
-                    🕒 Tổng thời gian thở oxy
-                </div>
-
-                <div style="font-size:34px; font-weight:bold; color:orange;">
-                    {tong_gio_oxy:.2f} GIỜ ({tong_phut_oxy} phút)
-                </div>
-
-                <br>
-
-                <div style="font-size:22px; color:#FFFFFF; font-weight:600;">
-                    ⏰ Giờ oxy (giờ thẳng)
-                </div>
-
-                <div style="font-size:42px; font-weight:bold; color:orange;">
-                    {ket_qua_oxy}
-                </div>
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
     # -------- PHẦN 2: NHIỀU NGÀY THỞ OXY (tính độc lập từng ngày) --------
     st.markdown("---")
     st.subheader("📋 NHIỀU NGÀY THỞ OXY (tính độc lập từng ngày)")
